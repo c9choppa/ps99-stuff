6ed645d38306f86d753bc962280d1aebcb5fab90
c9 <86535314+c9choppa@users.noreply.github.com>
2024-12-21 19:42:37 +0200

    Create ps99.lua

---
+++ ps99.lua
@@ -0,0 +1,52 @@
+local zones = {
+    {number = 1, name = "Spawn", price = 0}, 
+    {number = 2, name = "Colorful Forest", price = 900},
+    {number = 3, name = "Castle", price = 2500},
+    {number = 4, name = "Green Forest", price = 8000},
+    {number = 5, name = "Autumn", price = 20000},
+    {number = 6, name = "Cherry Blossom", price = 60000},
+    {number = 7, name = "Farm", price = 150000}, -- 50k Coins
+    {number = 8, name = "Backyard", price = 400000}, -- 400k Coins
+    {number = 9, name = "Misty Falls", price = 1000000}, -- 1 Gold Bar
+    {number = 10, name = "Mine", price = 2500000}, -- 2 Gold Bars + 500k Coins
+    {number = 11, name = "Chrystal Caverns", price = 6000000}, -- 6 Gold Bars
+    {number = 12, name = "Dead Forest", price = 15000000}, -- 15 Gold Bars
+    {number = 13, name = "Dark Forest", price = 35000000}, -- 35 Gold Bars
+    {number = 14, name = "Mushroom Field", price = 80000000}, -- 80 Gold Bars
+    {number = 15, name = "Enchanted Forest", price = 200000000}, -- 200 Gold Bars
+    {number = 16, name = "Crimson Forest", price = 450000000}, -- 450 Gold Bars
+    {number = 17, name = "Jungle", price = 1000000000}, -- 1,000 Gold Bars
+    {number = 18, name = "Jungle Temple", price = 2500000000}, -- 2,500 Gold Bars
+    {number = 19, name = "Oasis", price = 5500000000}, -- 5,500 Gold Bars
+    {number = 20, name = "Beach", price = 10000000000}, -- 10,000 Gold Bars
+    {number = 21, name = "Coral Reef", price = 30000000000}, -- 30,000 Gold Bars
+    {number = 22, name = "Shipwreck", price = 65000000000}, -- 65,000 Gold Bars
+    {number = 23, name = "Atlantis", price = 150000000000}, -- 150,000 Gold Bars
+    {number = 24, name = "Palm Beach", price = 350000000000}, -- 350,000 Gold Bars
+    {number = 25, name = "Tiki", price = 750000000000}, -- 750,000 Gold Bars
+    {number = 26, name = "Pirate Cove", price = 1500000000000}, -- 1 Platinum Coin + 500,000 Gold Bars
+    {number = 27, name = "Pirate Tavern", price = 4000000000000}, -- 4 Platinum Coins
+    {number = 28, name = "Shanty Town", price = 85000000000000}, -- 8 Platinum Coins + 500,000 Gold Bars
+    {number = 29, name = "Desert Village", price = 20000000000000}, -- 20 Platinum Coins
+    {number = 30, name = "Fossil Digsite", price = 45000000000000}, -- 45 Platinum Coins
+    {number = 31, name = "Desert Pyramids", price = 100000000000000}, -- 100 Platinum Coins
+    {number = 32, name = "Red Desert", price = 250000000000000}, -- 250 Platinum Coins
+    {number = 33, name = "Wild West", price = 500000000000000}, -- 500 Platinum Coins
+    {number = 34, name = "Grand Canyons", price = 1000000000000000}, -- 1,000 Platinum Coins
+    {number = 35, name = "Safari", price = 2500000000000000}, -- 2,500 Platinum Coins
+    {number = 36, name = "Mountains", price = 6000000000000000}, -- 6,000 Platinum Coins
+    {number = 37, name = "Snow Village", price = 15000000000000000}, -- 15,000 Platinum Coins
+    {number = 38, name = "Icy Peaks", price = 30000000000000000}, -- 30,000 Platinum Coins
+    {number = 39, name = "Ice Rink", price = 65000000000000000}, -- 65,000 Platinum Coins
+    {number = 40, name = "Ski Town", price = 150000000000000000}, -- 150,000 Platinum Coins
+    {number = 41, name = "Hot Springs", price = 350000000000000000}, -- 350,000 Platinum Coins
+    {number = 42, name = "Fire and Ice", price = 750000000000000000}, -- 750,000 Platinum Coins
+    {number = 43, name = "Volcano", price = 1500000000000000000}, -- 1 Platinum Bar + 500,000 Platinum Coins
+    {number = 44, name = "Obsidian Cave", price = 4000000000000000000}, -- 4 Platinum Bars
+    {number = 45, name = "Lava Forest", price = 8500000000000000000}, -- 8 Platinum Bars + 500,000 Platinum Coins
+    {number = 46, name = "Underworld", price = 20000000000000000000}, -- 20 Platinum Bars
+    {number = 47, name = "Underworld Bridge", price = 45000000000000000000}, -- 45 Platinum Bars
+    {number = 48, name = "Underworld Castle", price = 100000000000000000000}, -- 100 Platinum Bars
+    {number = 49, name = "Metal Dojo", price = 200000000000000000000}, -- 200 Platinum Bars
+    {number = 50, name = "Fire Dojo", price = 500000000000000000000}, -- 500 Platinum Bars
+}
